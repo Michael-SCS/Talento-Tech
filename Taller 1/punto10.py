5c28f2075f8439b427c8c8b652e770f4c295fa3e
Michael Steven Castillo <michaelsteven3211@gmail.com>
2024-09-16 20:48:59 -0500

    Se corrige el Ejercicio #10 con código proporcionado por Coordinador

--- Taller 1/punto10.py
+++ Taller 1/punto10.py
@@ -3,13 +3,28 @@ Escribir un programa que pregunte al usuario por las ventas de un rango de años
 
 import pandas as pd
 
-ventas = {"Año": [], "Ventas": [], "Ventas con descuento": []}
+# Definimos el Data Frame
+ventas = pd.DataFrame()
+
+# Segmentamos las listas para que sean más fáciles de utilizar
+año = []
+ventas_totales = []
+ventas_descuento = []
+
+# Pedimos los datos de inicio y fin del ciclo
 inicio = int(input("Por favor ingrese el año de inicio: "))
 fin = int(input("Por favor ingrese el año de fin: "))
+
+# Pedimos los datos de venta por año
 for i in range(inicio, fin+1):
-    ventas["Año"].append(i)
-    ventas["Ventas"].append(float(input(f"Por favor ingrese las ventas del año {i}: ")))
-    ventas["Ventas con descuento"].append(ventas["Ventas"][-1] * 0.9)
-print(ventas)
-ventas = pd.DataFrame(ventas)
+    año.append(i)
+    venta = float(input(f"Por favor ingrese las ventas del año {i}: ")) # Agregamos aparte el input
+    ventas_totales.append(venta) # Agregamos las ventas a la lista de ventas totales por año
+    ventas_descuento.append(venta * 0.9) # Multiplicamos por 0,9 para obtener el 90%, es decir, el descuento de 10%
+
+#Creamos las columnas y le asignamos los valores de las listas al Data Frame. 
+ventas['Año'] = año
+ventas['Ventas totales'] = ventas_totales
+ventas['Ventas con descuento'] = ventas_descuento
+
 print(ventas)
