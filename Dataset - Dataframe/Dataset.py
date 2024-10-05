cfc689be7a5a71e4278d8e4c481b51fa3ad4a5b7
Michael Steven Castillo <michaelsteven3211@gmail.com>
2024-10-04 19:31:10 -0500

    Dataframe y dataset

---
+++ Dataset - Dataframe/Dataset.py
@@ -0,0 +1,27 @@
+#Ejercicio de dataset
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#Cargar el Dataset
+dataset = pd.read_csv('datos.csv', delimiter=';')
+
+#resumen de los datos
+print('-------------Resumen estadistico-------------')
+print(dataset.describe())
+
+#Mostrar los primeros 5 registros
+print('-------------Primeros 5 registros-------------')
+print(dataset.head())
+
+#Gráfico de distribución de una variable numérica
+sns.histplot(dataset['Numero'])
+plt.show()
+
+#Gráfico de dispersión para ver la relación entre dos variables numéricas
+sns.scatterplot(x='Peso', y='Altura', data=dataset)
+plt.show()
+
+#Grafico de caja para identificar
+sns.boxplot(x='Categoria', y='Altura', data=dataset)
+plt.show()
---
+++ Dataset - Dataframe/Dataset2.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#Cargar el Dataset Titanic directamente desde seaborn
+df = sns.load_dataset('titanic')
+
+#mostrar las primeras filas
+print('-------------Primeros 5 registros-------------')
+print(df.head())
+
+#información del Dataframe
+print('-------------Información del Dataframe-------------')
+print(df.info())
+
+#Descripción estadistica
+print('-------------Resumen estadistico-------------')
+print(df.describe().round(2))
+
+#Verificar si hay valores nulos
+print('-------------Valores nulos-------------')
+print(df.isnull().sum())
+
+#Elimine filas con valores nulos
+df_cleaned = df.dropna()
+
+#Rellenar valores nulos en columnas
+df['age'].fillna(df['age'].mean(),inplace=True)
+
+#Verificar valores nulos después de la limpieza
+print('-------------Valores nulos despues de la limpieza-------------')
+print(df.isnull().sum())
+plt.figure(figsize=(8,6))
+sns.histplot(df['age'], kde=True,bins=30)
+plt.title('Distribución de la edad')
+plt.xlabel('Edad')
+plt.ylabel('Frecuencia')
+plt.show()
+plt.figure(figsize=(8,6))
+sns.scatterplot(x='age', y='fare', data=df)
+plt.title('Relación entre la edad y la tarifa')
+plt.show()
